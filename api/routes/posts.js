c56ee05f1e86305ca2be69532f2c1879e347a5f4
Pedro Wiliam Santos Comes Barros <pedro.barros@sipam.gov.br>
2023-05-09 10:44:31 -0300

    comentando scripts nao finalizados

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -1,6 +1,12 @@
 import express from "express";
+//import { addPost, deletePost, getPost, getPosts, updatePost } from "../controllers/posts";
 
 const router = express.Router()
-
-
+/*
+router.get("/", getPosts)
+router.get("/:id", getPost)
+router.post("/", addPost)
+router.delete("/:id", deletePost)
+router.update("/:id", updatePost)
+*/
 export default router
